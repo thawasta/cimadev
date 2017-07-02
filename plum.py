b5d28586acfcfceeb5a1db7049059b127a7b29b4
thawasta <thawasta@e-mail.dk>
2017-07-02 12:45:46 +0200

    Add files via upload

--- plum.py
+++ plum.py
@@ -1,11 +1,16 @@
 import requests as req
 import datetime
-import csv
 import sys
-site = 'www.google.com'
+
 siteinfo = []
-#for site in range(1, len(sys.argv)):
-r = req.post('https://plumbr.eu/robot/validateDomain', {'domainName': site})
-if (r.ok == True):
-    rs = req.post('https://plumbr.eu/robot/submit', {'domainName': site, 'reportEmail': 'andreas@cima.dk'})
-    dt = datetime.datetime.strftime('')
+sites = ['http://copenhagenevent.dk/', 'https://polterabend.dk/', 'http://www.copenhagenet.dk/']
+
+
+with open('plumdata.csv', 'ab') as pf:
+    for site in sites:
+        r = req.post('https://plumbr.eu/robot/validateDomain', {'domainName': site})
+        if (r.ok == True):
+            rs = req.post('https://plumbr.eu/robot/submit', {'domainName': site, 'reportEmail': 'andreas@cima.dk'})
+            print rs
+            dt = datetime.datetime.now().strftime('%d/%m/%y - %H.%M')
+            pf.write('%s, %s\n' % (site, dt))
